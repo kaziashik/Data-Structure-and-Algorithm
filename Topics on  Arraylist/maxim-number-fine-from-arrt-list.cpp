92df731438f1d2d02f33fa72de251736cde66595
kaziashik <kazia096@gmail.com>
2022-03-01 17:34:13 +0800

    maximum number finding from arry list

---
+++ Topics on  Arraylist/maxim-number-fine-from-arrt-list.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1,200,100,56};
+    int maxNumber=arr[0];
+    for(int i=1; i<5;  i++){
+        if(maxNumber<arr[i]){
+            maxNumber = arr[i];
+        }
+    }
+
+cout<< "max number is"  <<maxNumber;
+return 0;
+}
