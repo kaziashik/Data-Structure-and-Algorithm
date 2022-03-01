4f1d8059c0e00bce0da4c0f255b50bb1c57d4028
kaziashik <kazia096@gmail.com>
2022-03-01 15:13:04 +0800

    1D array

---
+++ Topics on  Arraylist/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+
+    string first_name, last_name;
+    cout<<"enter your first name:";
+    cin>>first_name;
+    cout<<"enter your last name";
+    cin>>last_name;
+
+    cout<<"your full name is " << first_name+last_name;
+} 
+
